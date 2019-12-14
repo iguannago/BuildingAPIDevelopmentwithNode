a1d4b09abd9c12e12c469ed15025d6ce8e95db12
david.crespo <david.crespo@hmcts.net>
2019-12-14 01:20:46 +0100

    in-built modules

--- moduleSystem/index.js
+++ moduleSystem/index.js
@@ -1,3 +1,10 @@
-const maths = require("./maths");
+const maths = require('./maths');
+const fs = require('fs');
 
 console.log(maths.add(2, 3));
+
+let file = `${__dirname}/in-file.txt`;
+fs.readFile(file, 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+})
