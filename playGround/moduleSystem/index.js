f90e99c90802a2219a9600f3f281fcfe611777db
david.crespo <david.crespo@hmcts.net>
2019-12-21 10:28:11 +0100

    move callback to constant to demostrate it better

--- playGround/moduleSystem/index.js
+++ playGround/moduleSystem/index.js
@@ -6,15 +6,23 @@ console.log('maths add function...');
 console.log(maths.add(2, 3));
 
 console.log('\nset timeout 1 sec delay...');
-setTimeout(()=> {
+setTimeout(() => {
     console.log(jsonFile.name);
     console.log(jsonFile.desc);
 }, 1000);
+
+const logMsg = () => {
+    console.log(jsonFile.name);
+    console.log(jsonFile.desc);
+};
+
+console.log('\nset timeout 1 sec delay callback...');
+setTimeout(logMsg, 1000);
+
 
 console.log('\nread a file and print it out...');
 const file = `${__dirname}/in-file.txt`;
 fs.readFile(file, 'utf8', (err, data) => {
     if (err) throw err;
     console.log(data);
-})    
-
+})
