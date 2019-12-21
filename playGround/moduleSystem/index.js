e74e468119e8562b5e718a04d8f2836a83a6f377
david.crespo <david.crespo@hmcts.net>
2019-12-21 10:21:25 +0100

    load json file directly

--- playGround/moduleSystem/index.js
+++ playGround/moduleSystem/index.js
@@ -1,10 +1,20 @@
 const maths = require('./maths');
 const fs = require('fs');
+const jsonFile = require('./json-file.json');
 
+console.log('maths add function...');
 console.log(maths.add(2, 3));
 
-let file = `${__dirname}/in-file.txt`;
+console.log('\nset timeout 1 sec delay...');
+setTimeout(()=> {
+    console.log(jsonFile.name);
+    console.log(jsonFile.desc);
+}, 1000);
+
+console.log('\nread a file and print it out...');
+const file = `${__dirname}/in-file.txt`;
 fs.readFile(file, 'utf8', (err, data) => {
     if (err) throw err;
     console.log(data);
-})
+})    
+
