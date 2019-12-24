f32a0582f6027d2fed11fbfc4cadfaf0b02f218a
david.crespo <david.crespo@hmcts.net>
2019-12-24 01:56:54 +0100

    add server configu

--- playGround/beginningApiDeveloment/settingUpHapi.js/server.js
+++ playGround/beginningApiDeveloment/settingUpHapi.js/server.js
@@ -1,22 +1,12 @@
-'use strict';
-
 const Hapi = require('hapi');
 
-const init = async () => {
-
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
-    });
-
-    await server.start();
-    console.log(`Server running on  ${server.info.uri}`);
-};
-
-process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
+const server = new Hapi.Server();
+server.connection({
+    host: 'localhost', 
+    port: '8080'
 });
 
-init();
+server.start((err) => {
+    if (err) throw err;
+    console.log(`Server running at ${server.info.uri}`);
+});
