4838a8d890af6a9fd3bdce3b4eb2f1028ac19e26
david.crespo <david.crespo@hmcts.net>
2019-12-24 02:00:56 +0100

    add hello world route

--- playGround/beginningApiDeveloment/settingUpHapi.js/server.js
+++ playGround/beginningApiDeveloment/settingUpHapi.js/server.js
@@ -6,6 +6,12 @@ server.connection({
     port: '8080'
 });
 
+server.route({
+    method: 'GET', 
+    path: '/', 
+    handler: (req, reply) => reply('Hello world!')
+})
+
 server.start((err) => {
     if (err) throw err;
     console.log(`Server running at ${server.info.uri}`);
